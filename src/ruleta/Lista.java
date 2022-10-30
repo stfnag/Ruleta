3e4c7f6a0bb3dc8a3fcd29cd5cc65e80384ec920
santiagoTrivi <santiagocarvajal103@gmail.com>
2022-10-30 16:52:00 -0400

    creacion de nuevo metodo

--- src/ruleta/Lista.java
+++ src/ruleta/Lista.java
@@ -80,7 +80,7 @@ public class Lista {
         Nodo aux = cabeza;        
         while (aux != null && this.participantes > 1) {
             
-            if (true != !aux.getParticipante().isArma() == true) // aumenta un num, y dice si disparo o no en base al arreglo boolean
+            if (aux.getParticipante().isArma() == true) // aumenta un num, y dice si disparo o no en base al arreglo boolean
             {
                 Nodo aux2 = aux;
                 this.eliminar(aux2);
